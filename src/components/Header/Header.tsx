6f91431bb4d030a357a3560a7a0d0ec1ce93c09f
Artem Eremchuk <eremchuk.it@gmail.com>
2024-02-11 14:34:56 +0300

    add react-icons in project

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,20 @@
 import styles from "./Header.module.scss";
 import { Link } from "react-router-dom";
 import Logo from "/public/images/icons/nodejs-logo.svg?react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
-const Header = () => {
+const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <div className="container">
         <div className={styles.header__wrapper}>
           <div className={styles.header__logo}>
-            <button className={styles.header__logo_btn}>
-              <Logo className={styles.header__logo_icon} />
+            <button className={styles.header__burger_btn}>
+              <GiHamburgerMenu className={styles.header__burger_icon} />
             </button>
-            <Link className={styles.header__logo_text} to="/nodejs-course">
-              NODE JS
+            <Link className={styles.header__logo} to="/nodejs-course/">
+              <Logo className={styles.header__logo_icon} />
+              <span className={styles.header__logo_text}>NODE JS</span>
             </Link>
           </div>
         </div>
